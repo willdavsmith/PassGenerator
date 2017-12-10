20c151a2a8511bf0ca48ee83e5556bf647ed1ee2
Will Smith <willdavsmith@gmail.com>
2017-12-09 22:14:06 -0500

    Initial Commit

---
+++ pwds.py
@@ -0,0 +1,6 @@
+import string, random
+
+def passgenerator (amount):
+	while (amount > 0):
+		print(''.join(random.sample(random.sample(string.digits, 10)+random.sample(string.lowercase, 10)+random.sample(string.uppercase, 10), 10)))
+		amount = amount - 1
