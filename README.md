c131865c656793031e346bf0dc0aadd9e184dcf6
Will Smith <willdavsmith@gmail.com>
2017-12-09 22:21:53 -0500

    Final Update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Generates a list of passwords of length 10 from a random pool of numbers, lowercase letters, and uppercase letters.
+
+	Example Output:
+
+> passgenerator(5)
+	> EV5weg9SU4
+	> Wjd9E20h1C
+	> sr9I2TJQKz
+	> VnE1D8pdLs
+	> 3vfyDG0iIU
--- pwds.py
+++ pwds.py
@@ -4,3 +4,6 @@ def passgenerator (amount):
 	while (amount > 0):
 		print(''.join(random.sample(random.sample(string.digits, 10)+random.sample(string.lowercase, 10)+random.sample(string.uppercase, 10), 10)))
 		amount = amount - 1
+
+
+passgenerator(5)
